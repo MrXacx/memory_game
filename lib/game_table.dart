418dfe8062f343aefabedc83c71d69baa7516f5e
MrXacx <misterxacx@gmail.com>
2023-11-21 10:45:52 -0300

    feat: cria métodos de controle do tabuleiro

--- lib/game_table.dart
+++ lib/game_table.dart
@@ -3,6 +3,7 @@ import 'game_card.dart';
 
 class GameTable extends StatelessWidget {
   final _icons = <IconData>[Icons.plus_one];
+  final state = _GameTable();
 
   GameTable({super.key}) {
     _icons.addAll(_icons); // Duplica ícones
@@ -17,4 +18,25 @@ class GameTable extends StatelessWidget {
           (index) => GameCard(
               icon: _icons[index]) // Emite uma carta para cada ícone da lista
           ));
+}
+
+class _GameTable {
+  final cardStack = GameCardStack();
+
+  bool isEquals() => cardStack.get(0) == cardStack.get(1);
+  bool compare(GameCard card, int index) => card == cardStack.get(index);
+}
+
+class GameCardStack {
+  final _stack = <GameCard>[];
+
+  void add(GameCard card) =>
+      _stack.length < 2 ? _stack.add(card) : throw Exception("Pilha cheia");
+
+  GameCard get(int index) =>
+      _stack.isNotEmpty ? _stack[index] : throw Exception("Pilha vazia");
+
+  void clear() => _stack.clear();
+
+  int length() => _stack.length;
 }
